26e7aff130d398f18f67f26e9030756f4238554f
Helena Zubčeková <heli.zubcekova@gmail.com>
2021-04-25 10:21:26 +0200

    changePage functionality

--- kviz.js
+++ kviz.js
@@ -15,47 +15,72 @@ let questions = [
     }
 ];
 
-let questionsNumber = questions.length;
 let i = 0;
+const questionsNumber = questions.length;
+setQuizPage(i);
 
-let questionPart = document.createElement("h2");
-let header = document.createElement("div");
-header.setAttribute("id", "poradi");
-header.innerHTML = `Otázka ${i + 1}/${questionsNumber}`;
-let question = document.createElement("div");
-question.setAttribute("id", "otazka");
-question.innerHTML = questions[i].question;
-questionPart.appendChild(header);
-questionPart.appendChild(question);
+function setQuizPage(i) {
 
-let photoPart = document.createElement("foto");
-let questionPhoto = document.createElement("img");
-questionPhoto.setAttribute("id", "obrazek");
-questionPhoto.src = questions[i].photo;
-photoPart.appendChild(questionPhoto);
+    let questionPartH2 = document.createElement("h2");
+    let headerPoradi = document.createElement("div");
+    headerPoradi.setAttribute("id", "poradi");
+    headerPoradi.innerHTML = `Otázka ${i + 1}/${questionsNumber}`;
+    let questionOtazka = document.createElement("div");
+    let questionText = questions[i].question;
+    questionOtazka.setAttribute("id", "otazka");
+    questionOtazka.innerHTML = questionText;
+    questionPartH2.appendChild(headerPoradi);
+    questionPartH2.appendChild(questionOtazka);
 
-let answersPart = document.createElement("div");
-answersPart.setAttribute("id", "moznosti");
-let answers = document.createElement("ul");
-answers.setAttribute("id", "odpovedi");
-answersPart.appendChild(answers)
-setAnswer()
+    let photoPartFoto = document.createElement("foto");
+    let obrazekImg = document.createElement("img");
+    obrazekImg.setAttribute("id", "obrazek");
+    obrazekImg.src = questions[i].photo;
+    photoPartFoto.appendChild(obrazekImg);
 
-let quiz = document.querySelector(".kviz");
-quiz.appendChild(questionPart);
-quiz.appendChild(photoPart);
-quiz.appendChild(answersPart);
+    let moznostiDiv = document.createElement("div");
+    moznostiDiv.setAttribute("id", "moznosti");
+    let odpovediUl = document.createElement("ul");
+    odpovediUl.setAttribute("id", "odpovedi");
+    moznostiDiv.appendChild(odpovediUl)
+    setAnswer(i, odpovediUl)
 
-function setAnswer() {
-    let answer;
+    let quiz = document.querySelector(".kviz");
+    quiz.appendChild(questionPartH2);
+    quiz.appendChild(photoPartFoto);
+    quiz.appendChild(moznostiDiv);
+};
+
+function setAnswer(i, odpovediUl) {
+    let answerText;
     for (let j = 0; j < questions[i].answers.length; j++) {
-        answer = questions[i].answers[j];
-        let answerPart = document.createElement("li");
-        answerPart.setAttribute("data-odpoved", j.toString());
-        answerPart.innerHTML = answer;
-        answers.appendChild(answerPart);
+        answerText = questions[i].answers[j];
+        let answerLi = document.createElement("li");
+        answerLi.setAttribute("data-odpoved", j.toString());
+        answerLi.innerHTML = answerText;
+        odpovediUl.appendChild(answerLi);
+        answerLi.addEventListener("click", goToOtherPage);
+    };
+};
+
+function goToOtherPage() {
+    i++;
+    clearPage();
+    if (i < questionsNumber) {
+        // reapply all
+        setQuizPage(i);
+    } else {
+        console.log("hey");
     }
-}
+};
+
+function clearPage() {    
+    quiz = document.querySelector(".kviz");
+    while (quiz.firstChild) {
+        quiz.removeChild(quiz.firstChild);
+    };
+};
+
 /*
 Při vytváření HTML pro odpovědi dodrž následující strukturu:
 
